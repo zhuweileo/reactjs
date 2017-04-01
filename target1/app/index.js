b2ef4194d9e84824d7ead7b63593399ec13a9a81
zhuwei <zhuwei_leo@163.com>
2017-04-01 14:28:48 +0800

    引入jquery

--- target1/app/index.js
+++ target1/app/index.js
@@ -1,11 +1,15 @@
 import _ from "lodash"
+import $ from "jquery"
 function component () {
-  var element = document.createElement('div');
+  // var element = document.createElement('div');
+  var element = $("<div></div>")
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  // element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.html(_.join(["Hello","webpack"]," "))
 
-  return element;
+  // return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
